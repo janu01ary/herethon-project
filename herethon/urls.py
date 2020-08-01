cdca8efe305c31911949aa2ca4b281222175502c
Your Name <you@example.com>
2020-08-01 21:46:16 +0900

    commit

--- herethon/urls.py
+++ herethon/urls.py
@@ -4,7 +4,7 @@ import hereapp.views
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),git 
     path('', hereapp.views.index,name='home'),
     path('here/', include('hereapp.urls')),
     path('accounts/', include('accounts.urls'))python manage.py runserver
