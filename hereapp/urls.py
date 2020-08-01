88bdf231eb4034e595c43b9fe915d5a0e7f91c0c
aaa <janu01ary@gmail.com>
2020-08-01 20:21:19 +0900

    mypage

--- hereapp/urls.py
+++ hereapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('addemail/', views.addemail, name='addemail'),
+    path('mypage/', views.mypage, name="mypage"),
+    path('showresults/', views.showresults, name='showreulsts')
 ]
--- hereapp/views.py
+++ hereapp/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Email
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def showresults(request):
+    return render(request, 'see-results.html')
+
+def addemail(request):
+    if request.method == 'POST':
+        newEmail = Email()
+        newEmail.name = request.POST['name']
+        newEmail.email = request.POST['email']
+        newEmail.save()
+        return redirect('mypage')
+    else:
+        return render(request, 'addemail.html')
+
+def mypage(request):
+    emails = Email.objects.all()
+    return render(request, 'mypage.html', {'emails':emails})
